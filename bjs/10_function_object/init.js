3447447a841d7d12b0f243c08d839a4cc77981ff
BlackTM0 <131348981+BlackTM0@users.noreply.github.com>
2023-07-30 16:39:30 +1000

    Delete init.js

--- bjs/10_function_object/init.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function()
-{
-    const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-    document.getElementById('surnameOutput').innerText = initPerson.surname;
-    document.getElementById('genderOutput').innerText = initPerson.gender;
-};
-
